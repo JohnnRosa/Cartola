511a3abe8bcc0abe89487d14dedd35df6cedaa16
YURI GRESPAN NEGHERBON <yuri_negherbon@estudante.sesisenai.org.br>
2023-05-29 22:12:38 -0300

    feat: add component and logic system

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import { useState,useEffect } from 'react'
 import './App.css'
+import Jogador from './components/Jogadores';
 
 function App() {
+  const [jogadores, setJogadores] = useState();
+
   useEffect(()=> {
 
     fetch('https://api.cartola.globo.com/atletas/pontuados/1').then(async(jogadores) => {
-    let data = await jogadores.json()
-    
-    console.log(data)
-    })
+      let data = await jogadores.json()
 
+      var atletas = Object.values(data.atletas);
+      var clubes = Object.values(data.clubes);
+      var posicoes = Object.values(data.posicoes);
+      
+      var cards = atletas.map(atleta =>{
+        return {
+          apelido: atleta.apelido,
+          pontuacao: atleta.pontuacao,
+          foto: atleta.foto,
+          escudo: clubes.filter(clube => clube.id == atleta.clube_id)[0].escudos['30x30'],
+          abreviacao:  clubes.filter(clube => clube.id == atleta.clube_id)[0].abreviacao,
+          posicao: posicoes[atleta.posicao_id-1].nome,
+        }
+      })
+      setJogadores(cards)
+    })
   }, [])
 
 
   return (
     <>
-      
+      {
+        jogadores?.map((atleta, i) =>{
+          return (
+            <Jogador 
+              key={i} 
+              apelido={atleta.apelido}
+              pontuacao={atleta.pontuacao}
+              foto={atleta.foto}
+              escudo={atleta.escudo}
+              abreviacao={atleta.abreviacao}
+              posicao={atleta.posicao}/> 
+          )
+        })
+      }
     </>
   )
 }
--- src/components/Jogadores.jsx
+++ src/components/Jogadores.jsx
@@ -1,13 +1,15 @@
-export default function Jogador ()
+export default function Jogador(jogador)
 {
-
     return(
-
         <div>
-            
+            <p>Nome: {jogador.apelido}</p>
+            <p>Pontuação: {jogador.pontuacao}</p>
+            <img src={jogador?.foto.replace('FORMATO','220x220')} width="60px" height="60px"/>
+            <p>Escudo: {jogador.escudo}</p>
+            <p>Abreviação: {jogador.abreviacao}</p>
+            <p>Posição: {jogador.posicao}</p>
+            <hr />
         </div>
-
-
     )
 
 }
